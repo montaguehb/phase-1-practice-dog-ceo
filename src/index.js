5e37ba8ce2d731c4d3268ad064e0d1b3e2489d54
Historia Montague <historia.montague@outlook.com>
2023-04-19 10:00:36 -0600

    Finished Lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('%c HI', 'color: firebrick')
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(imageObj => imageObj.message.forEach(element => {
+        const image = document.createElement("img")
+        const dogBreedLi = document.createElement("li")
+        image.src = element
+        dogBreedLi.textContent = element.split("/")[element.split("/").indexOf("breeds") + 1]
+        if(dogBreedLi.textContent.charAt(0) !== document.querySelector("#breed-dropdown").value ) {
+            dogBreedLi.hidden = true;
+        }
+        dogBreedLi.addEventListener("click", event => {
+            event.target.style.color = "#ffb7c5"
+        })
+        document.querySelector("#dog-image-container").append(image)
+        document.querySelector("#dog-breeds").append(dogBreedLi)
+    }));
+
+    
+    document.addEventListener("change", event => {
+        debugger
+        document.querySelectorAll("li").forEach(element => {
+            element.textContent.charAt(0) === event.target.value ? element.hidden = false:element.hidden = true;
+        })
+    })
+});
